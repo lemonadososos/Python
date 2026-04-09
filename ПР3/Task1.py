f5ec61bbca4c300eea9b09f51dd736e5e752b012
lemonadososos <lemonadososos>
2026-04-09 15:53:12 +0300

    upd

---
+++ ПР3/Task1.py
@@ -0,0 +1,18 @@
+a = 42
+b = 3.14159
+c = "Hello, Python!"
+d = True
+e = [1, 2, 3]
+f = (4, 5, 6)
+g = {"name": "Alice", "age": 30}
+h = {7, 8, 9}
+i = None
+print("a имеет ",type(a))
+print("b имеет ",type(b))
+print("c имеет ",type(c))
+print("d имеет ",type(d))
+print("e имеет ",type(e))
+print("f имеет ",type(f))
+print("g имеет ",type(g))
+print("h имеет ",type(h))
+print("i имеет ",type(i))
