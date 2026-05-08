d65802b9804a380213557a9e90ba2a209d13c7a0
lemonadososos <lemonadososos>
2026-05-08 14:52:03 +0300

    upd

---
+++ ПР8/Task6.py
@@ -0,0 +1,23 @@
+import random
+
+print("угадайте число от 1 до 10")
+
+number = random.randint(1, 10)
+num = 0
+popitka = 1
+
+while num != number and popitka <= 3:
+    num = int(input(f"попытка {popitka}: "))
+    if num == number:
+        print("поздравляю! вы угадали")
+        break
+    else:
+        print("неправильное число")
+        if popitka < 3:
+
+            if num > number:
+                print("число меньше")
+            else:
+                print("число больше")
+        else:
+            print("попытки кончились")
