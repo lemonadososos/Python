fc465e84202a22625d1cd78b087e252a1d9afab8
Lemonadososos <Huawei@DESKTOP-91KJA31>
2026-06-09 13:56:55 +0300

    upd

---
+++ Prez_DO-WHILE/Task_2.py
@@ -0,0 +1,8 @@
+proizvedenie = 1
+
+while True:
+    num = int(input("введите число для умножения на общее произведение(вначале 1): "))
+    if num <= 0:
+        break
+    proizvedenie *= num
+    print(proizvedenie)
